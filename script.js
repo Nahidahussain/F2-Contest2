d83f2ba823fab3dfd0b61a8fc246aed632f3e35b
Nahida Hussain <89774747+Nahidahussain@users.noreply.github.com>
2023-06-24 22:53:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-// Function to fetch data using .then
+// Fetch data using .then
 function fetchDataWithThen() {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
       .then(response => response.json())
-      .then(data => renderTable(data))
+      .then(data => {
+        renderTable(data);
+        filterTable();
+      })
       .catch(error => console.error(error));
   }
   
-  // Function to fetch data using async/await
+  // Fetch data using async/await
   async function fetchDataWithAsyncAwait() {
     try {
       const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false');
       const data = await response.json();
       renderTable(data);
+      filterTable();
     } catch (error) {
       console.error(error);
     }
   }
   
-  // Function to render the table
+  // Render the table
   function renderTable(data) {
     const table = document.getElementById('dataTable');
     table.innerHTML = '';
@@ -38,7 +42,7 @@ function fetchDataWithThen() {
     });
   }
   
-  // Function to filter the table based on search input
+  // Filter the table based on search input
   function filterTable() {
     const input = document.getElementById('searchInput').value.toUpperCase();
     const table = document.getElementById('dataTable');
@@ -50,7 +54,7 @@ function fetchDataWithThen() {
     }
   }
   
-  // Function to sort the table based on the given key
+  // Sort the table based on the given key
   function sortTable(key) {
     const table = document.getElementById('dataTable');
     const rows = Array.from(table.getElementsByTagName('tr')).slice(1);
@@ -70,4 +74,7 @@ function fetchDataWithThen() {
     // Reinsert the sorted rows into the table
     rows.forEach(row => table.appendChild(row));
   }
+  
+  // Initial fetch using .then method
+  fetchDataWithThen();
   
